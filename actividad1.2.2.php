d75d6bf1c3d007512fb55793c46dbd374855118b
Ximena0606 <242310432@itslerdo.edu.mx>
2025-06-09 23:34:29 -0600

    Create actividad1.2.2.php

---
+++ actividad1.2.2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Mi primera página PHP</title>
+</head>
+<body>
+  <h1>Bienvenido a mi primera página con PHP</h1>
+  <p>Hola Mundo desde PHP</p>
+  <p>Este texto está escrito en HTML, pero el mensaje anterior fue generado con PHP.</p>
+</body>
+</html>
